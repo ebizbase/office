5bd8ce94badc8a660079a15994e16c6fb5bb21fa
John Martin <john.itvn@gmail.com>
2024-12-12 18:27:14 +0000

    refactor: create testing-common library for provide testing utilities

---
+++ apps/services/mailer-service-e2e/src/mailer-service/main.spec.ts
@@ -0,0 +1,130 @@
+import {
+  MailHogContainer,
+  StartedRabbitMQContainer,
+  RabbitMQContainer,
+  StartedMailHogContainer,
+  MailerServiceContainer,
+  StartedMailerServiceContainer,
+} from '@ebizbase/testing-common';
+import { MongoDBContainer, StartedMongoDBContainer } from '@testcontainers/mongodb';
+
+describe('Main spec', () => {
+  let mongodbContainer: StartedMongoDBContainer;
+  let rabbitmqContainer: StartedRabbitMQContainer;
+  let mailhogContainer: StartedMailHogContainer;
+  let mailerServiceContainers: StartedMailerServiceContainer[];
+
+  beforeAll(async () => {
+    const infraContainers = await Promise.all([
+      new MongoDBContainer().start(),
+      new RabbitMQContainer().start(),
+      new MailHogContainer().start(),
+    ]);
+    mongodbContainer = infraContainers[0];
+    rabbitmqContainer = infraContainers[1];
+    mailhogContainer = infraContainers[2];
+    mailerServiceContainers = [];
+  }, 30000);
+
+  beforeEach(async () => {
+    for (const mailerServiceContainer of mailerServiceContainers) {
+      await mailerServiceContainer.stop();
+    }
+  });
+
+  it('should send email success full', async () => {
+    const mailerServiceContainer = await new MailerServiceContainer({
+      mongodb: mongodbContainer,
+      rabitmq: rabbitmqContainer,
+      mailhog: mailhogContainer,
+    }).start();
+    mailerServiceContainers.push(mailerServiceContainer);
+
+    const mailToSent = {
+      from: 'noreply@example.com',
+      to: 'test1@example.com',
+      subject: 'Test Email',
+      text: 'This is a test email',
+      html: '<p>This is a test email</p>',
+    };
+    (await rabbitmqContainer.getAmqpClient()).sendToQueue(
+      'send_email_queue',
+      Buffer.from(JSON.stringify(mailToSent))
+    );
+    const receivedMails = await mailhogContainer.waitForEmail(mailToSent.to);
+    expect(receivedMails).toHaveLength(1);
+    expect(receivedMails[0].Content.Headers['Subject'][0]).toBe(mailToSent.subject);
+  }, 10000);
+
+  it('should send email after service restart', async () => {
+    const mailToSent = {
+      from: 'noreply@example.com',
+      to: 'test2@example.com',
+      subject: 'Test Email',
+      text: 'This is a test email',
+      html: '<p>This is a test email</p>',
+    };
+
+    const mailerServiceContainer = await new MailerServiceContainer({
+      mongodb: mongodbContainer,
+      rabitmq: rabbitmqContainer,
+      mailhog: mailhogContainer,
+    }).start();
+    mailerServiceContainers.push(mailerServiceContainer);
+
+    (await rabbitmqContainer.getAmqpClient()).sendToQueue(
+      'send_email_queue',
+      Buffer.from(JSON.stringify(mailToSent))
+    );
+
+    await new MailerServiceContainer({
+      mongodb: mongodbContainer,
+      rabitmq: rabbitmqContainer,
+      mailhog: mailhogContainer,
+    }).start();
+
+    const receivedMails = await mailhogContainer.waitForEmail(mailToSent.to);
+    expect(receivedMails).toHaveLength(1);
+    expect(receivedMails[0].Content.Headers['Subject'][0]).toBe(mailToSent.subject);
+  }, 10000);
+
+  it('should send email with multiple instances', async () => {
+    const mailToSent = {
+      from: 'noreply@example.com',
+      to: 'test3@example.com',
+      subject: 'Test Email',
+      text: 'This is a test email',
+      html: '<p>This is a test email</p>',
+    };
+
+    mailerServiceContainers = await Promise.all([
+      new MailerServiceContainer({
+        mongodb: mongodbContainer,
+        rabitmq: rabbitmqContainer,
+        mailhog: mailhogContainer,
+      }).start(),
+      new MailerServiceContainer({
+        mongodb: mongodbContainer,
+        rabitmq: rabbitmqContainer,
+        mailhog: mailhogContainer,
+      }).start(),
+    ]);
+
+    (await rabbitmqContainer.getAmqpClient()).sendToQueue(
+      'send_email_queue',
+      Buffer.from(JSON.stringify(mailToSent))
+    );
+
+    await new MailerServiceContainer({
+      mongodb: mongodbContainer,
+      rabitmq: rabbitmqContainer,
+      mailhog: mailhogContainer,
+    })
+      .withReuse()
+      .start();
+
+    const receivedMails = await mailhogContainer.waitForEmail(mailToSent.to);
+    expect(receivedMails).toHaveLength(1);
+    expect(receivedMails[0].Content.Headers['Subject'][0]).toBe(mailToSent.subject);
+  }, 10000);
+});
---
+++ libs/testing-common/src/mailer-service/mailer-container.ts
@@ -0,0 +1,47 @@
+import { StartedRabbitMQContainer } from '@testcontainers/rabbitmq';
+import { StartedMongoDBContainer } from '@testcontainers/mongodb';
+import {
+  AbstractStartedContainer,
+  GenericContainer,
+  StartedTestContainer,
+  Wait,
+} from 'testcontainers';
+import { StartedMailHogContainer } from '../mailhog';
+
+export class MailerServiceContainer extends GenericContainer {
+  constructor(
+    infras: {
+      mongodb: StartedMongoDBContainer;
+      rabitmq: StartedRabbitMQContainer;
+      mailhog: StartedMailHogContainer;
+    },
+    image?: string
+  ) {
+    super(image || 'ebizbase/office-mailer-service:edge');
+    this.withEnvironment({
+      MONGODB_URI: `${infras.mongodb.getConnectionString()}?directConnection=true`,
+      RABBITMQ_URIS: infras.rabitmq.getAmqpUrl(),
+      SMTP_URI: infras.mailhog.getSmtpUri(),
+    });
+    this.waitStrategy = Wait.forLogMessage('Mailer service is up and running');
+  }
+
+  override async start(): Promise<StartedMailerServiceContainer> {
+    const staredContainer = await super.start();
+    return new StartedMailerServiceContainer(staredContainer);
+  }
+}
+
+export class StartedMailerServiceContainer extends AbstractStartedContainer {
+  constructor(container: StartedTestContainer) {
+    super(container);
+  }
+
+  getSmtpUri(): string {
+    return 'smtp://' + this.getHost() + ':' + this.getMappedPort(1025);
+  }
+
+  getMailhogApiUrl(): string {
+    return `http://localhost:${this.getMappedPort(8025)}/api`;
+  }
+}
